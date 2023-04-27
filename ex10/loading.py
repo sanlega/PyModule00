191b69d013f05af847f61f522c7578b97eb7ee03
Santi Legaristi Royo <slegaris@c1r12s4.42madrid.com>
2023-04-27 09:28:26 +0200

    modifications to 04 05 10

--- ex10/loading.py
+++ ex10/loading.py
@@ -6,35 +6,24 @@
 #    By: slegaris <slegaris@student.42madrid.com>   +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/17 22:56:27 by slegaris          #+#    #+#              #
-#    Updated: 2023/04/17 23:38:06 by slegaris         ###   ########.fr        #
+#    Updated: 2023/04/18 11:58:52 by slegaris         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 from tqdm import tqdm
 import time
-import sys
 
-def ft_progress(lst):
-    total = len(lst)
-    start_time = time.time()
-
-    initial_sample = 10
-    for i in range(initial_sample):
-        time.sleep(0.01)
-    initial_eta = start_time * (total / initial_sample)
-
-    with tqdm(total=total, unit="elem", ncols=80, mininterval=0.1, maxinterval=0.5, bar_format="ETA: {eta}s {l_bar}{bar}| {n_fmt}/{total_fmt} | elapsed time {elapsed_s:3.2f}s", dynamic_ncols=True, ascii=" >=") as pbar:
-        pbar.set_postfix(eta=f"{initial_eta:.0f}", refresh=False)
-        pbar.refresh()
-        time.sleep(0.1)
-
-        for item in lst:
-            yield item
-            elapsed = time.time() - start_time
-            if pbar.n != 0:
-                pbar.set_postfix(eta=f"{elapsed * (total - pbar.n) / pbar.n:.0f}", refresh=False)
-            pbar.update(1)
-            time.sleep(0.01)
+def ft_progress(listy):
+    return tqdm(listy, 
+                iterable="=>",
+                ncols=80, 
+                # mininterval=0.1, 
+                # maxinterval=0.5, 
+                bar_format='{l_bar}{bar}{r_bar}',
+                ascii=' >=',
+                smoothing=0.8,
+                colour='green',
+                unit_scale=True,)
 
 listy = range(1000)
 ret = 0
